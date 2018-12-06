74eebf626353538e0fa9a37e96538024cb99241f
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-06 14:42:33 -0800

    get current position

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback
+    (position) => console.log(position),
+    // failure callback
+    (err) => console.log(err)
+  );
+
   return <div>Hi There!</div>;
 };
 
