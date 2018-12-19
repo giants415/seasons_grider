a080894dfd60e4e42cc3f4fe3730ac7b20a91b83
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-18 16:24:32 -0800

    error handling basically

--- src/index.js
+++ src/index.js
@@ -4,21 +4,32 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { lat: null };
+    
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
 
     window.navigator.geolocation.getCurrentPosition(
       // success callback
-      (position) => {
+      position => {
         this.setState({ lat: position.coords.latitude });
       },
       // failure callback
-      (err) => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   render () {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
