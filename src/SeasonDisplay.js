a13a6677e7d6851965b81b87ae474b14460be356
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-18 17:15:19 -0800

    creates function to get season data

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    // terenary expression
+    return lat > 0 ? 'summer' : 'winter';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
   return <div>Season here</div>;
 };
 
