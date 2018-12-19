13a2a2b8d7b988923337b02fb1b67968bb384670
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-18 17:23:19 -0800

    variable for choosing icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,6 +12,7 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const displayText = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
     <div>
